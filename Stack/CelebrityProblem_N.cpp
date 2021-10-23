8008ed44eabd18b47346835b0a895d038c92828f
Akash Chouhan <akash.c1500@gmail.com>
2021-10-24 02:49:20 +0530

    Update CelebrityProblem_N.cpp

--- Stack/CelebrityProblem_N.cpp
+++ Stack/CelebrityProblem_N.cpp
@@ -1,6 +1,6 @@
 /*Problem Statement: A celebrity is a person who is known to all but does not know anyone at a party. If you go to a party of N people, find if there is a celebrity in the party or not.
 A square NxN matrix M[][] is used to represent people at the party such that if an element of row i and column j  is set to 1 it means ith person knows jth person. Here M[i][i] will always be 0.
-[Below is the Optimsed Approach to this Problem -> O(N^2) Time]
+[Below is the Optimsed Approach to this Problem -> Linear O(N) Time]
 
 Sample Test Case:
 3
